6ed0d7666fa4972057245d222006f18f38355671
aysonsteven <aysonsteven@gmail.com>
2017-01-18 19:36:38 +0800

    work on find-id

--- src/components/modals/find-id/find-id.ts
+++ src/components/modals/find-id/find-id.ts
@@ -26,7 +26,7 @@ export class FindIdModal implements OnInit{
 
   onClickFindID(){
       this.loading = true;
-      this.user.get( this.email.replace('@', '+').replace('.', '+'), res =>{
+      this.user.get( this.email.replace('@', '+').replace('.', '-'), res =>{
           console.log('res ' + JSON.stringify(res))        
             this.user.get( res.uid , re =>{
                 
