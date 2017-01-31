751f67c072f8c530805a9f33b8b27e9c5a4f744d
manueljun17 <manuelmanansalajr@gmail.com>
2017-01-31 18:38:45 +0800

    youtube

--- src/components/teacher/teacher.ts
+++ src/components/teacher/teacher.ts
@@ -6,5 +6,22 @@ import { LMS } from '../../providers/lms';
 })
 export class TeacherComponent {
     @Input() teachers;
-    constructor( public lms: LMS ) {}
+    playYoutube:boolean = false;
+    constructor( public lms: LMS ) {
+    }
+    ngOnChanges(changes) {
+        if(changes['teachers']) {
+            if(!this.teachers) return;
+            console.log(this.teachers)
+            this.teachers.forEach( (teacher) => {
+                if( teacher.url_youtube.match('^http://')){
+                    teacher.url_youtube = teacher.url_youtube.replace(/^http:\/\//i, 'https://');
+                }
+                if( teacher.url_youtube.match(/youtu.be/g)){
+                    teacher.url_youtube = teacher.url_youtube.replace(/youtu.be/g, 'youtube.com/embed');
+                }
+                console.log(teacher);
+            });
+        }
+    }
 }
