9cbd1548e8ac00ae9233400b1973527904a093cf
manueljun17 <manuelmanansalajr@gmail.com>
2017-01-31 22:06:55 +0800

    youtube video

--- src/components/teacher/teacher.ts
+++ src/components/teacher/teacher.ts
@@ -1,26 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { LMS } from '../../providers/lms';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
     selector: 'teacher-component',
     templateUrl: 'teacher.html'
 })
 export class TeacherComponent {
     @Input() teachers;
-    playYoutube:boolean = false;
-    constructor( public lms: LMS ) {
+    playVideo:boolean = false;
+    constructor( public lms: LMS,public sanitizer: DomSanitizer) {
     }
     ngOnChanges(changes) {
         if(changes['teachers']) {
             if(!this.teachers) return;
             console.log(this.teachers)
             this.teachers.forEach( (teacher) => {
-                if( teacher.url_youtube.match('^http://')){
-                    teacher.url_youtube = teacher.url_youtube.replace(/^http:\/\//i, 'https://');
-                }
-                if( teacher.url_youtube.match(/youtu.be/g)){
-                    teacher.url_youtube = teacher.url_youtube.replace(/youtu.be/g, 'youtube.com/embed');
-                }
-                console.log(teacher);
+                teacher.play_video = false;
+                if( teacher.url_youtube.match('^http://')) teacher.url_youtube = teacher.url_youtube.replace(/^http:\/\//i, 'https://');//replace http to https
+                if( teacher.url_youtube.match(/youtu.be/g)) teacher.url_youtube = teacher.url_youtube.replace(/youtu.be/g, 'youtube.com/embed');//replace youtu.be to youtube.com/embed
+                teacher.url_youtube = teacher.url_youtube + "?autoplay=1";
+                teacher.url_youtube = this.sanitizer.bypassSecurityTrustResourceUrl(teacher.url_youtube );//to fix unsafe
             });
         }
     }
