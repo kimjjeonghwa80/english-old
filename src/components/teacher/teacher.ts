a06cb1f7b358526cdff9296fb299247a27c916ea
manueljun17 <manuelmanansalajr@gmail.com>
2017-02-06 21:00:47 +0800

    update teacher

--- src/components/teacher/teacher.ts
+++ src/components/teacher/teacher.ts
@@ -21,6 +21,7 @@ export class TeacherComponent {
                 teacher.play_video = false;
                 if( teacher.url_youtube.match('^http://')) teacher.url_youtube = teacher.url_youtube.replace(/^http:\/\//i, 'https://');//replace http to https
                 if( teacher.url_youtube.match(/youtu.be/g)) teacher.url_youtube = teacher.url_youtube.replace(/youtu.be/g, 'youtube.com/embed');//replace youtu.be to youtube.com/embed
+                if( teacher.greeting.match(/<img[^>]*>/g)) teacher.greeting = teacher.greeting.replace(/<img[^>]*>/g,"");
                 teacher.url_youtube = teacher.url_youtube + "?autoplay=1";
                 teacher.url_youtube = this.sanitizer.bypassSecurityTrustResourceUrl(teacher.url_youtube );//to fix unsafe
             });
