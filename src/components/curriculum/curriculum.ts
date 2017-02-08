668a46e48c60f1576880461adb5ca620ec63489b
manueljun17 <manuelmanansalajr@gmail.com>
2017-02-08 15:54:12 +0800

    update course component

--- src/components/curriculum/curriculum.ts
+++ src/components/curriculum/curriculum.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
+import * as xInterface from '../../app/app.interface';
 @Component({
     selector: 'curriculum-component',
     templateUrl: 'curriculum.html'
 })
 export class CurriculumComponent {
+    temp:any = [];
     showBook:boolean = false;
+    bookList:xInterface.BOOKLIST = xInterface.bookList;
     constructor() {
-
+        this.takeSomeTemporaryBooks();
+    }
+    takeSomeTemporaryBooks() {
+        this.temp = this.bookList.books;
+        this.bookList.books = _.take( this.bookList.books, 8);
+    }
+    onClickShowMore() {
+        this.showBook =!this.showBook;
+        if(this.showBook) {
+            this.bookList.books = this.temp;
+        }
+        else {
+            this.takeSomeTemporaryBooks();
+        }
     }
 }
