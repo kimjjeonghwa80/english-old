e2fe995a0fef61e8246543d997f15a489c02d1a1
aysonsteven <aysonsteven@gmail.com>
2017-02-09 17:14:36 +0800

    applying lms codes

--- src/providers/lms.ts
+++ src/providers/lms.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 export const LMS_URL = "http://onlineenglish.kr";
 export const LMS_ENDPOINT_URL = LMS_URL + "/ajax.php";
+export const domain: string = 'englishfordevelopers.onlineenglish.kr';
 export interface TEACHER {
     birthday: string;
     classid: string;
@@ -39,6 +40,48 @@ export class LMS {
 
             console.log(json);
             success( json['data'] );
+        });
+    }
+
+    register( data, success, failure: ( error : string ) => void ){
+        let url = LMS_ENDPOINT_URL + `?id=${data['id']}&name=${data['name']}&nickname=${data['nickname']}&email=${data['email']}&mobile=${data['mobile']}&classid=${data['classid']}&domain=${domain}&domain_key=empty&function=user_insert`;
+
+        this.http.get( url ).subscribe( re =>{
+            console.log( ' user_insert :: ' + re );
+            if( re ) success( re );
+            else failure( ' error on lms registration ' );
+        })
+    }
+
+    update( data, success, failure: ( error: string) => void ){
+        let url = LMS_ENDPOINT_URL + `?id=${data['id']}&name=${data['name']}&nickname=${data['nickname']}&email=${data['email']}&mobile=${data['mobile']}&classid=${data['classid']}&domain=${domain}&domain_key=empty&function=user_update`;
+
+        this.http.get( url ).subscribe( re =>{
+            console.log( ' user_update :: ' + re );
+            if( re ) success( re );
+            else failure( ' error on lms update user ' );
+        })
+    }
+
+    getReservations( data, success ) {
+        let url = LMS_ENDPOINT_URL + `?id=${data['id']}&name=${data['name']}&nickname=${data['nickname']}&email=${data['email']}&mobile=${data['mobile']}&classid=${data['classid']}&domain=${domain}&domain_key=empty&function=reservation_list`;
+        console.log('url: ', url);
+        this.http.get( url ).subscribe( re =>{
+            let json = null;
+            try {
+                json = JSON.parse( re['_body'] );
+            }
+            catch ( e ) {
+                alert("Parse ERROR on lms::getTeachers()");
+            }
+
+            if ( json['code'] ) {
+                alert( json['message'] );
+            }
+            else {
+                console.log(json);
+                success( json['data'] );
+            }
         });
     }
 }
