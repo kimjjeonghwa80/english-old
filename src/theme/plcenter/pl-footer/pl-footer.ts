5d8c7e31501e9b929ae9a0978a807a74ce8c3862
Manuel Manansala <manueljun17@gmail.com>
2017-02-28 21:20:33 +0800

    update pl-footer

--- src/theme/plcenter/pl-footer/pl-footer.ts
+++ src/theme/plcenter/pl-footer/pl-footer.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { App } from '../../../providers/app';
 @Component({
     selector: 'pl-footer-component',
     templateUrl: 'pl-footer.html'
 })
 export class PlFooterComponent {
+    constructor( private app: App) {}
+    onClickPanelMenu( name ) {
+        this.app.scrollTo( name );
+    }
 }
