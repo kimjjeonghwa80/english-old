d2b90db900762800228915b0a119734012037096
Manuel Manansala <manueljun17@gmail.com>
2017-03-02 20:21:19 +0800

    kkang header

--- src/theme/kkang/kkang-footer/kkang-footer.ts
+++ src/theme/kkang/kkang-footer/kkang-footer.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { App } from '../../../providers/app';
 @Component({
     selector: 'kkang-footer-component',
     templateUrl: 'kkang-footer.html'
 })
 export class KkangFooterComponent {
-    constructor( ) {}
+    constructor( private app: App) {}
+    onClickPanelMenu( name ) {
+        this.app.scrollTo( name );
+    }
 }
